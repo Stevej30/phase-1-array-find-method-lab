7b808dacad92b7190fb037373b2a52ee58be0ae4
Stevan Jacob <stevanjacob@gmail.com>
2021-09-22 13:44:22 -0400

    commit tree 7b65324661a4efecbf66c78922625d632c1d270b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+// will loop through array
+function loopThrough(answer, finder){
+    let thisThing = null;
+    for(let item of answer){
+        if(finder(item) === true){
+            thisThing = item;
+            break;
+        }
+    }
+}
 
+//will find superbowl win
+
+function superbowlWin(final){
+    return answer.final === "W";
+}
